7e9af60550fc12dca3b9f9bff56bffc3a4b06870
khalilanReza <khalilan.reza@gmail.com>
2022-03-28 16:53:15 +0430

    adding framerMotion

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,9 @@
 import { Card } from "./styles/Card";
+import { motion } from "framer-motion";
 
 const Movie = ({ movie }) => {
   return (
-    <Card bg={movie.backdrop_path}>
+    <Card as={motion.div} layout bg={movie.backdrop_path}>
       <img
         src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}
         alt={movie.title || movie.name}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Movie from "./Movie";
 import { MovieContainer } from "./styles/MovieContainer";
+import { motion } from "framer-motion";
 
 const MovieList = ({ movies }) => {
   return (
-    <MovieContainer>
+    <MovieContainer as={motion.div} layout>
       {movies.map((movie) => (
         <Movie movie={movie} key={movie.id} />
       ))}
